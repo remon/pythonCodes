2c88cff6d3572a42dcd0eaa412897033c964b9aa
Remon <remmony@gmail.com>
2018-06-29 18:26:23 +0200

    add strings_example_en.py file

---
+++ Strings/strings_example_en.py
@@ -0,0 +1,22 @@
+#This file contains some examples for Python Strings 
+#What is Strings in python ?
+#Strings are amongst the most popular types in Python.
+#We can create them simply by enclosing characters in quotes. 
+#Python treats single quotes the same as double quotes. 
+#Creating strings is as simple as assigning a value to a variable.
+#source of Definition https://www.tutorialspoint.com/python/python_strings.htm
+
+#example 1
+#added by @remon
+str1 ="Hello World"
+print(str1)
+
+#example 2
+#added by @remon
+str2 ='Hello World 2'
+print(str2)
+
+#example 3
+#added by @remon
+str3 ="2018"
+print(str3)
