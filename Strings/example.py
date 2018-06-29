c0837b67307cce7184ea0930589e82c80c2d785a
sundus <to_learning@yahoo.com>
2018-06-30 01:39:15 +0300

    put my file in string folder

---
+++ Strings/example.py
@@ -0,0 +1,9 @@
+string="ali ahmed salm"
+print (string.find("sa"))
+print (string.find(""))
+print (string.find (" "))
+print (string.find("ah",3,5))
+print (string.find("ah",3,6))
+def get_s(a, b):
+  return a[:b]
+print(get_s("mySchool",10))
