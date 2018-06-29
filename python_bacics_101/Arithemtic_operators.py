78566167f0b2ee2e4b51f68f31bb1aa711ffa7a4
MenaKamal <anglejob_2710@hotmail.com>
2018-06-30 00:31:00 +0300

    python101: adding operators with description

--- python_bacics_101/Arithemtic_operators.py
+++ python_bacics_101/Arithemtic_operators.py
@@ -1,4 +1,26 @@
-# Examples on how to uses python operators 
+# Examples on how to uses Python Arithmetic Operators
+'''
+What is the operator in Python?
+Operators are special symbols in Python that carry out arithmetic or logical computation.
+The value that the operator operates on is called the operand.
+for example: 2 + 3 = 5 
+Here, + is the operator that performs addition.
+2 and 3 are the operands and 5 is the output of the operation.
+
+what is Arithmetic Operators means ? 
+	Operator 			| Description
+	---------------------------
+	+ Addition 			| Adds values on either side of the operator.
+	- Subtraction 		| Subtracts right hand operand from left hand operand.
+	* Multiplication 	| Multiplies values on either side of the operator 
+	/ Division 			| Divides left hand operand by right hand operand 
+	% Modulus 			| Divides left hand operand by right hand operand and returns remainder
+	** Exponent			| Performs exponential (power) calculation on operators
+	// 	Floor Division	| he division of operands where the result is the quotient in which the digits after the decimal point are removed. But if one of the operands is negative, the result is floored, i.e., rounded away from zero
+
+when do we use it ? 
+we use this kind of every where form basic math operation to loops or condition statements 
+''' 
 a = 20 ; b = 10
 
 # Addition operator
@@ -29,7 +51,7 @@ print "Exponent value = " , c
 # floor Division or integer division operator
 '''
 Note : 
-In Python the division of 5 / 2 will return 2.5 this is floating point division 
+In Python 3 the division of 5 / 2 will return 2.5 this is floating point division 
 the floor Division or integer divisio will return 2 mean return only the integer value 
 '''
 a = 9 ; b = 4 
--- python_bacics_101/Assignment_operators.py
+++ python_bacics_101/Assignment_operators.py
@@ -1,10 +1,23 @@
 # Examples on how to uses python Assignments operators 
 '''
 Assignment operators means:
-adding a new value to variable
-like increase value / decrease / multiply and more
+adding a new value to variable , ike increase value / decrease / multiply and more
+
+		Operator 		| Description
+	---------------------------
+	= equal		 		| Assigns values from right side operands to left side operand for example: a = 10 or c = a + b assigns value of a + b into c
+	+= Add AND			| It adds right operand to the left operand and assign the result to left operand
+	-= Subtract AND 	| It subtracts right operand from the left operand and assign the result to left operand
+	*= Multiply AND		| It multiplies right operand with the left operand and assign the result to left operand
+	/= Divide AND		| It divides left operand with the right operand and assign the result to left operand
+	%= Modulus AND		| It takes modulus using two operands and assign the result to left operand
+	**= Exponent AND	| Performs exponential (power) calculation on operators and assign value to the left operand
+	//= Floor Division	| It performs floor division on operators and assign value to the left operand
+
+when do we use it ? 
 we usually use this kind of assignments in loops or condition statements
 ''' 
+
 a = 2 ; b = 1 ; c = 0
 print "values = ", "a=" , a , "b=" ,  b , "c=" , c 
 
