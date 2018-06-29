f3ab82a50698a6422a88eb757a85756b65dfd823
shorouq saad <shrooqabdullahsaad@gamil.com>
2018-06-29 22:47:31 +0300

    add list_example_en.py file

---
+++ Lists/list_example_en.py
@@ -0,0 +1,14 @@
+#This file contains some examples for Python Lists
+#How to create lists
+
+#example 1
+#added by @engshorouq
+#empty list
+my_list=[]
+
+#list of integers
+my_list=[1,2,3,4,5]
+
+#list with mixed datatype
+my_list=[2,"python",2.2]
+
