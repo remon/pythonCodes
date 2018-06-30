f7080bd936cca83545347e5b9a965fe83c4d1498
Dima almasri <dima_saderose1@hotmail.com>
2018-06-30 10:18:36 +0300

    OOP Examples

---
+++ OOP/Classes_en.py
@@ -0,0 +1,33 @@
+#declare a class
+
+#Synatx
+  #class ClassName:
+  #block of code
+
+#Example 1
+# __init__  passing initial values to your object
+
+class Car:
+    
+    'Print Car Name And Price'
+
+    def __init__(self,CarName,Price):
+           self.CarName = CarName
+           self.Price = Price
+       
+NewCar = Car("I8", 200000)
+
+print  "Car Name " + NewCar.CarName + "  Car Price " + str(NewCar.Price)
+
+
+print "Car.__doc__:", Car.__doc__
+
+
+
+             
+                
+    
+        
+        
+
+ 
