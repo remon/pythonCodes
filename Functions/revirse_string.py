7c97fb12704d233f1f4cdea996c3d78912d6675f
Azhar MMA <azharo@msn.com>
2018-07-02 12:50:22 +0300

    Add Reverse String Function

---
+++ Functions/revirse_string.py
@@ -0,0 +1,11 @@
+def revirse_string(word):
+    s=""
+    word= word.split()
+    for e in word:
+        s+= e[::-1] + " "
+    print s
+
+
+word="Welcome to pythonCodes on GitHub"
+
+revirse_string(word)
