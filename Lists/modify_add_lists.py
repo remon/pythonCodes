45afd0d4984c352f82f5258faa1912eff86c186e
shorouq saad <shrooqabdullahsaad@gamil.com>
2018-07-03 15:05:27 +0300

    add element to the lists using append

---
+++ Lists/modify_add_lists.py
@@ -0,0 +1,16 @@
+#how to add element to lists
+#how to modify elements in the lists
+
+#example1
+#added by @engshorouq
+#create empty list
+my_list=[]
+
+#add element to empty string in two ways the first way(append)
+my_list.append("Learn")
+my_list.append(["Python","Language"])
+
+#print list to show the output
+print(my_list)
+
+
