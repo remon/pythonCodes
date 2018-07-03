850068ea67295920c50459863fa33baa4610f1fa
basmalah-kamal <basmalakamal4444@gmail.com>
2018-07-03 22:33:04 +0200

    add a file for built-in functions

---
+++ Functions/built-in-functions.py
@@ -0,0 +1,106 @@
+"""
+This file contains some examples for built-in functions in python
+they are functions that do certain things 
+I will explain 
+"""
+
+
+#example 1 = .upper()
+#added by @basmalakamal
+str1 = "hello world"
+print str1.upper()
+#this how we use it and it is used to return the string with capital letters
+
+#example 2 = isupper()
+#added by @basmalakamal
+str2 = "I love coding"
+print str2.isupper()
+#this function returns a boolean it is used to see if all letters in the string are capital I have only one capital letter here 
+#so it returned False
+#lets try again here
+str3 = "PYTHON IS AWESOME"
+print str3.isupper() #prints True
+
+#example 3 = lower()
+#added by @basmalakamal
+str4 = "BASMALAH"
+print str4.lower()
+#it returns the string with small letters
+
+#example 4 = islower()
+#added by @basmalakamal
+str5 = "Python"
+print str5.islower()
+#this function returns a boolean it is used to see if all letters in the string are small I have one capital letter here 
+#so it returned False
+#lets try again here
+str6 = "python"
+print str6.islower() #prints True
+
+#example 5  = float()
+#added by @basmalakamal
+print float('12\n')
+#we can use this one to input an integer and it will return a float depends on what you have written 
+#for example I wrote 12\n (\n means a space) so it returned 12.0
+
+
+#example 6 = id()
+#added by @basmalakamal
+print id(str6)
+#it returns the identity of any object cause every object has an identity in python memory
+
+#example 7 = len()
+#added by @basmalakamal
+
+
+
+
+
+#example 8 = int()
+#added by @basmalakamal
+
+
+
+#example 9 = str()
+#added by @basmalakamal
+
+
+
+
+#example 10 = max()
+#added by @basmalakamal
+
+
+
+#example 11 = sort()
+#added by @basmalakamal
+
+
+#example 12 = replace()
+#added by @basmalakamal
+
+
+#example 13 = append()
+#added by @basmalakamal
+
+#example 14 = index()
+#added by @basmalakamal
+
+#example 15 = split()
+#added by @basmalakamal
+
+#example 16 = join()
+#added by @basmalakamal
+
+
+
+
+
+
+
+
+
+
+
+
+
