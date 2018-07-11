0400fb11fe24081e047fef3088b0a47dd04e720b
Heba-Ahmad-Saada <hebasaada@msn.com>
2018-07-12 01:17:00 +0300

    Add the order of operations for boolean operators

--- True and False/Boolean_Operators_en.py
+++ True and False/Boolean_Operators_en.py
@@ -4,7 +4,8 @@
 #1-and, checks if both the statements are True; 2-or, checks if at least one of the statements is True;
 #3-not, gives the opposite of the statement.
 #the table bellow shows the results of using AND, OR, and NOT boolean operators:
-
+#Boolean operators aren't just evaluated from left to right. Just like with arithmetic operators, there's an order of operations for boolean operators:
+#1-not is evaluated first; 2-and is evaluated next; 3-or is evaluated last.
 boolean_operator= {"row1 c1": "  A  ", "row1 c2": "  B  ", "row1 c3": "A AND B", "row1 c4": "     ", "row1 c5": "  A  ", "row1 c6": "  B  ", "row1 c7": " A OR B", "row1 c8": "      ", "row1 c9": "  A  ", "row1 c10": "NOT A ","row2 c1": "True ", "row2 c2": "True ", "row2 c3": " True  ", "row2 c4": "     ", "row2 c5": "True ", "row2 c6": "True ", "row2 c7": " True  ", "row2 c8": "      ", "row2 c9": "True ", "row2 c10": "False", "row3 c1": "True ", "row3 c2": "False", "row3 c3": " False ", "row3 c4": "     ", "row3 c5": "True ", "row3 c6": "False", "row3 c7": " True  ", "row3 c8": "      ", "row3 c9": "False", "row3 c10": "True ","row4 c1": "False", "row4 c2": "True ", "row4 c3": " False ", "row4 c4": "     ", "row4 c5": "False", "row4 c6": "True ", "row4 c7": " True  ", "row4 c8": "      ", "row4 c9": "      ", "row4 c10": "      ", "row5 c1": "False", "row5 c2": "False", "row5 c3": " False ", "row5 c4": "     ", "row5 c5": "False", "row5 c6": "False", "row5 c7": " False ", "row5 c8": "      ", "row5 c9": "      "}
 def display_board(boolean_operator):
     bord = """
@@ -21,3 +22,7 @@ def display_board(boolean_operator):
     print bord
 
 display_board(boolean_operator)
+order_for_boolean= "\n".join(["1- not is evaluated first"
+,"2- and is evaluated next","3- or is evaluated last."])
+print "The order of operations for boolean operators:"
+print (order_for_boolean)
