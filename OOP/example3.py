33c94a11c8c4705763007b233dc4f392dfc01aa1
Mostafa Ibrahim <mimobibo442@gmail.com>
2018-08-04 10:17:31 +0200

    add some OOP examples

---
+++ OOP/example3.py
@@ -0,0 +1,16 @@
+class calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+
+    def sub(self):
+        return self.a - self.b
+
+    def mul(self):
+        return self.a * self.b
+
+    def div(self):
+        return self.a / self.b
